5b8bbe0ddbfea1c6d5c8d6eeb9fd43e2321f45f2
nightusk <nightusk@gmail.com>
2024-09-08 10:20:34 +0900

    mod keys

--- users/nightusk/nightusk.h
+++ users/nightusk/nightusk.h
@@ -4,19 +4,28 @@
 
 #define LAYOUT_ortho_3x10_wrapper(...) LAYOUT_ortho_3x10(__VA_ARGS__)
 
+// R2
+#define MT_A    LSFT_T(     KC_A)
+#define MT_SCLN RSFT_T(     KC_SCLN)
 // R1
+#define MT_Z    LCTL_T(     KC_Z)
+#define MT_X    LGUI_T(     KC_X)
+#define MT_C    LALT_T(     KC_C)
 #define LT_B    LT(_60____, KC_B)
 #define LT_N    LT(_60_s__, KC_N)
+#define MT_M    RCTL_T(     KC_M)
+#define MT_COMM RGUI_T(     KC_COMM)
+#define MT_DOT  RALT_T(     KC_DOT)
 
 // Minimal
 #define __QWERT____________________________________ KC_Q,    KC_W,    KC_E,    KC_R,    KC_T
 #define __YUIOP____________________________________ KC_Y,    KC_U,    KC_I,    KC_O,    KC_P
-#define __ASDFG____________________________________ KC_A,    KC_S,    KC_D,    KC_F,    KC_G
+#define __ASDFG____________________________________ MT_A,    KC_S,    KC_D,    KC_F,    KC_G
 #define __HJKL____________________________          KC_H,    KC_J,    KC_K,    KC_L
-#define __ZXCVB____________________________________ KC_Z,    KC_X,    KC_C,    KC_V,    LT_B
-#define __NM____________                            LT_N,    KC_M
-#define __SCLN_QUOT_____                            KC_SCLN, KC_QUOT
-#define __COMM_DOT__SLSH_________                   KC_COMM, KC_DOT,  KC_SLSH
+#define __ZXCVB____________________________________ MT_Z,    MT_X,    MT_C,    KC_V,    LT_B
+#define __NM____________                            LT_N,    MT_M
+#define __SCLN_QUOT_____                            MT_SCLN, KC_QUOT
+#define __COMM_DOT__SLSH_________                   MT_COMM, MT_DOT,  LT_SLSH
 // 60%
 #define __12345____________________________________ KC_1,    KC_2,    KC_3,    KC_4,    KC_5
 #define __67890____________________________________ KC_6,    KC_7,    KC_8,    KC_9,    KC_0
